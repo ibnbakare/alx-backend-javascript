2c4d8a09151fff4b7007ed0b17980a874f5ad177
ridhwan <ridmartrite@gmail.com>
2023-04-22 00:20:36 +0100

    adding task 8

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(Name) {
+    this._name = Name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(Code) {
+    this._code = Code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
