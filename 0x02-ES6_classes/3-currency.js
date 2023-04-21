2f291c44eb8aadf25a4869048274ab26fca901f7
ridhwan <ridmartrite@gmail.com>
2023-04-21 22:56:16 +0100

    adding task 4

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code === 'string') {
+      throw new TypeError('code must be strings');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name}(${this._code})`;
+  }
+}
